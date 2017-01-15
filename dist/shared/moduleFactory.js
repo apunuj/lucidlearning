aaea75d87227bb58cbe1b0db181cb5301c8f60d2
Apoorv Kesharwani <Apoorv@macs-MacBook-Air.local>
2017-01-16 02:22:13 +0530

    second commit

--- dist/shared/moduleFactory.js
+++ dist/shared/moduleFactory.js
@@ -2,7 +2,7 @@
 
 angular.module('clientApp')
 
-.constant('baseUrl', 'http://localhost:3000/')
+.constant('baseUrl', 'http://ec2-54-172-201-85.compute-1.amazonaws.com:3000/')
 
 .factory('moduleFactory', ['$resource', 'baseUrl', function($resource, baseUrl){
     return $resource(baseUrl+'modules/:id', null, {'update': {method: 'PUT'}});
