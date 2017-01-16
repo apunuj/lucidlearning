3408e464d90eb675875d8415de048237c8395f92
Apoorv Kesharwani <Apoorv@macs-MacBook-Air.local>
2017-01-16 20:48:47 +0530

    Made Modifications to the modify structure page

--- dist/components/newModule/CreateCtrl.js
+++ dist/components/newModule/CreateCtrl.js
@@ -3,16 +3,39 @@
 angular.module('clientApp')
 
 .controller('CreateCtrl', ['$scope', '$state', '$stateParams', 'moduleFactory', 'topicFactory', 'learningPointFactory', function($scope, $state, $stateParams, moduleFactory, topicFactory, learningPointFactory) {
+    
     $scope.module = moduleFactory.get({id: $stateParams.id})
                     .$promise.then(function(response){
                         $scope.module = response;
+                        $scope.module.newTopicObject = {
+                            name: "",
+                            learningPoints: []
+                        };
+                        for (var index = 0; index < $scope.module.topics.length; index++) {
+                            $scope.module.topics[index].newLearningPointObject = {
+                                name: "",
+                                content:""
+                            };
+                        }
+                        $scope.showTopicForm = false;
+                        $scope.toggleTopicForm = function(){
+                            $scope.showTopicForm = !$scope.showTopicForm;
+                        }
+                        
+                        $scope.showLearningPoint = [];
+                        for (var index = 0; index < $scope.module.topics.length; index++) {
+                            $scope.showLearningPoint.push(false);
+                        }
+                        $scope.toggleShowLearningPoint = function(tindex) {
+                            $scope.showLearningPoint[tindex] = !$scope.showLearningPoint[tindex];
+                        }
                     },
                     function(response){
                         console.log(response.status);
                     });
 
     
-    $scope.removeTopic = function(topicId){
+    $scope.removeTopic = function(topicId) {
         topicFactory.remove({id:$stateParams.id, tid: topicId})
         .$promise.then(function(response){
             console.log(response);
@@ -22,10 +45,42 @@ angular.module('clientApp')
             console.log(response.status);
         });
     };
-    $scope.removeLearningPoint = function(topicId, lpId){
+    
+    $scope.removeLearningPoint = function(topicId, lpId) {
         learningPointFactory.remove({id:$stateParams.id, tid: topicId, lid: lpId})
         .$promise.then(function(response){
             console.log(response);
+            $state.go($state.current, {}, {reload: true});
+        },
+        function(response){
+            console.log(response.status);
+        });
+    };
+
+    $scope.addTopic = function() {
+        topicFactory.save({id:$stateParams.id},$scope.module.newTopicObject)
+        .$promise.then(function(response){
+            console.log(response);
+            $scope.module.newTopicObject = {
+                name: "",
+                learningPoints: []
+            };
+            $scope.showTopicForm = false;
+            $state.go($state.current, {}, {reload: true});
+        },
+        function(response){
+            console.log(response.status);
+        });
+    };
+
+    $scope.addLearningPoint = function(tindex) {
+        learningPointFactory.save({id:$stateParams.id, tid:$scope.module.topics[tindex]._id},$scope.module.topics[tindex].newLearningPointObject)
+        .$promise.then(function(response){
+            console.log(response);
+            $scope.module.topics[tindex].newLearningPointObject = {
+                name: "",
+                content: ""
+            };
             $state.go($state.current, {}, {reload: true});
         },
         function(response){
--- dist/shared/moduleFactory.js
+++ dist/shared/moduleFactory.js
@@ -2,8 +2,11 @@
 
 angular.module('clientApp')
 
-.constant('baseUrl', 'http://ec2-54-172-201-85.compute-1.amazonaws.com:3000/')
+.constant('baseUrl', 'http://localhost:3000/')
 
 .factory('moduleFactory', ['$resource', 'baseUrl', function($resource, baseUrl){
     return $resource(baseUrl+'modules/:id', null, {'update': {method: 'PUT'}});
 }]);
+
+//http://localhost:3000/
+//http://ec2-54-172-201-85.compute-1.amazonaws.com:3000/
