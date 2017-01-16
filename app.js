68655b0f7e32319a92827629e0a37dde8417de7e
Apoorv Kesharwani <Apoorv@macs-MacBook-Air.local>
2017-01-16 13:48:25 +0530

    Updated for responsive design and accessing through local host

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next(); 
+});
 
 /**
  * Development Settings
