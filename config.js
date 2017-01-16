576045d3d164b8b8875325d5414ec8621dbaaa08
Apoorv Kesharwani <Apoorv@macs-MacBook-Air.local>
2017-01-16 10:47:35 +0530

    Updated bitnami mongo url

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://Ap00Rv:Apunuj@ds155418.mlab.com:55418/lucid'
+    'mongoUrl': 'mongodb://Alpesh:Lucid1@ec2-54-172-201-85.compute-1.amazonaws.com:27017/dummyDB'
 };
 
-//Local and Mlab sandbox dbs:
+//Local and Mlab sandbox dbs, and bitnami too:
 //mongodb://macs-MacBook-Air.local:27017/lucid
 //mongodb://Ap00Rv:Apunuj@ds155418.mlab.com:55418/lucid
+//mongodb://{NEW USERNAME}:{NEW PASSWORD}@{EC2 URL}:{PORT}/dummyDB
+//mongodb://Alpesh:Lucid1@ec2-54-172-201-85.compute-1.amazonaws.com:27017/dummyDB
