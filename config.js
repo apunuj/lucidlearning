39845820e8a11dd5d2d2766fe29f1e24777965ca
Apoorv Kesharwani <Apoorv@macs-MacBook-Air.local>
2017-03-09 01:25:14 +0530

    deploying with atlas

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': 'kenRobinson',
-    'mongoUrl': 'mongodb://Ap00Rv:Apunuj@ds155418.mlab.com:55418/lucid'
+    'mongoUrl': 'mongodb://ApunujLucid:6aEck25kHtLIz4q5@cluster0-shard-00-00-pvgll.mongodb.net:27017,cluster0-shard-00-01-pvgll.mongodb.net:27017,cluster0-shard-00-02-pvgll.mongodb.net:27017/lucid?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 };
 
 //Local and Mlab sandbox dbs, and bitnami too:
